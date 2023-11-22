085787ab14885a0b0dbcbc25398f1d199b365c7a
theProgrammingBox <justinboxgea@gmail.com>
2023-11-21 17:52:54 -0800

    progress

--- Source4.cu
+++ Source4.cu
@@ -69,6 +69,31 @@ int compareFloats(const void* a, const void* b) {
   return (fa > fb) - (fa < fb);
 }
 
+__global__ void compareTensors(const float *tensorA, const float *tensorB, uint32_t size, bool *result) {
+    uint32_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= size) return;
+
+    if (fabsf(tensorA[idx] - tensorB[idx]) > 0.001f) {
+        *result = false;
+    }
+}
+
+bool areTensorsEqual(float *dTensorA, float *dTensorB, uint32_t size) {
+    bool *dResult, hResult = true;
+    cudaMalloc(&dResult, sizeof(bool));
+    cudaMemcpy(dResult, &hResult, sizeof(bool), cudaMemcpyHostToDevice);
+
+    uint32_t threadsPerBlock = 256;
+    uint32_t blocks = (size + threadsPerBlock - 1) / threadsPerBlock;
+    
+    compareTensors<<<blocks, threadsPerBlock>>>(dTensorA, dTensorB, size, dResult);
+
+    cudaMemcpy(&hResult, dResult, sizeof(bool), cudaMemcpyDeviceToHost);
+    cudaFree(dResult);
+    
+    return hResult;
+}
+
 __global__ void matrixMul(const float *A, const float *B, float *C, 
                           int M, int N, int K) {
     int tx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -82,15 +107,48 @@ __global__ void matrixMul(const float *A, const float *B, float *C,
     }
 }
 
+template <int BLOCK>
+__global__ void sgemm(int m, int n, int k, float *a, int lda, float *b, int ldb,
+                      float *c, int ldc) {
+  const int tx = threadIdx.x;
+  const int ty = threadIdx.y;
+  const int bx = blockIdx.x;
+  const int by = blockIdx.y;
+
+  float *begin_a = a + by * BLOCK * k;
+  float *begin_b = b + bx * BLOCK;
+  float *end_a = begin_a + k;
+
+  float sum = 0.f;
+  for (float *a_ptr = begin_a, *b_ptr = begin_b; a_ptr < end_a;
+       a_ptr += BLOCK, b_ptr += BLOCK * n) {
+    __shared__ float ashare[BLOCK][BLOCK];
+    __shared__ float bshare[BLOCK][BLOCK];
+
+    ashare[ty][tx] = a_ptr[ty * k + tx];
+    bshare[ty][tx] = b_ptr[ty * n + tx];
+    __syncthreads();
+
+#pragma unroll
+    for (int kk = 0; kk < BLOCK; ++kk) {
+      sum += ashare[ty][kk] * bshare[kk][tx];
+    }
+    __syncthreads();
+  }
+
+  c[(BLOCK * by + ty) * n + BLOCK * bx + tx] = sum;
+}
+
+
 int main() {
   uint32_t seed1, seed2;
   initializeSeeds(&seed1, &seed2);
 
   const uint32_t widthA = 1024;
-  const uint32_t heightA = 1024; 
-  const uint32_t widthC = 1024;
+  const uint32_t heightA = 2048; 
+  const uint32_t widthC = 512;
   
-  float *dTensorA, *dTensorB, *dTensorC;
+  float *dTensorA, *dTensorB, *dTensorC, *dTensorCCublas;
   
   cublasHandle_t handle;
   checkCublasStatus(cublasCreate(&handle));
@@ -98,6 +156,7 @@ int main() {
   cudaMalloc(&dTensorA, widthA * heightA * sizeof(float));
   cudaMalloc(&dTensorB, widthC * widthA * sizeof(float)); 
   cudaMalloc(&dTensorC, heightA * widthC * sizeof(float));
+  cudaMalloc(&dTensorCCublas, heightA * widthC * sizeof(float));
   
   fillDTensor(dTensorA, widthA * heightA, &seed1, &seed2);
   fillDTensor(dTensorB, widthC * widthA, &seed1, &seed2);
@@ -105,7 +164,7 @@ int main() {
   // printDTensor(dTensorA, widthA, heightA, "A");
   // printDTensor(dTensorB, widthC, widthA, "B");
   
-  const uint32_t samples = 1024;
+  const uint32_t samples = 1024 * 4;
   
   float times[samples];
   
@@ -116,29 +175,27 @@ int main() {
   
   float mean, median;
   
+  
+  
   for (uint32_t i = samples; i--;) {
 
     gettimeofday(&t_start, NULL);
   
-    checkCublasStatus(cublasSgemm(
+    cublasSgemm(
       handle, CUBLAS_OP_N, CUBLAS_OP_N,
       widthC, heightA, widthA,
       &alpha,
       dTensorB, widthC,
       dTensorA, widthA,
       &beta,
-      dTensorC, widthC));
-    
-    // dim3 threads(32, 32);
-    // dim3 blocks((widthC + threads.x - 1) / threads.x, (heightA + threads.y - 1) / threads.y);
-    // matrixMul<<<blocks, threads>>>(dTensorA, dTensorB, dTensorC, heightA, widthC, widthA);
+      dTensorCCublas, widthC);
       
     gettimeofday(&t_end, NULL);
 
     times[i] = (t_end.tv_sec - t_start.tv_sec) * 1000.0f + (t_end.tv_usec - t_start.tv_usec) / 1000.0f;
   }
     
-  // printDTensor(dTensorC, widthC, heightA, "C");
+  // printDTensor(dTensorCCublas, widthC, heightA, "C");
   
   mean = 0.0f;
   for (uint32_t i = samples; i--;) mean += times[i];
@@ -150,18 +207,11 @@ int main() {
   printf("Mean: %f ms\n", mean);
   printf("Median: %f ms\n", median);
   
+  
+  
   for (uint32_t i = samples; i--;) {
 
     gettimeofday(&t_start, NULL);
-  
-    // checkCublasStatus(cublasSgemm(
-    //   handle, CUBLAS_OP_N, CUBLAS_OP_N,
-    //   widthC, heightA, widthA,
-    //   &alpha,
-    //   dTensorB, widthC,
-    //   dTensorA, widthA,
-    //   &beta,
-    //   dTensorC, widthC));
     
     dim3 threads(32, 32);
     dim3 blocks((widthC + threads.x - 1) / threads.x, (heightA + threads.y - 1) / threads.y);
@@ -172,7 +222,7 @@ int main() {
     times[i] = (t_end.tv_sec - t_start.tv_sec) * 1000.0f + (t_end.tv_usec - t_start.tv_usec) / 1000.0f;
   }
     
-  // printDTensor(dTensorC, widthC, heightA, "C");
+  areTensorsEqual(dTensorC, dTensorCCublas, heightA * widthC) ? printf("\nTensors are equal\n") : printf("\nTensors are not equal\n");
   
   mean = 0.0f;
   for (uint32_t i = samples; i--;) mean += times[i];
@@ -183,10 +233,40 @@ int main() {
   
   printf("Mean: %f ms\n", mean);
   printf("Median: %f ms\n", median);
+  
+  
+  
+  for (uint32_t i = samples; i--;) {
 
+    gettimeofday(&t_start, NULL);
+    
+    dim3 threads(32, 32);
+    dim3 blocks((widthC + threads.x - 1) / threads.x, (heightA + threads.y - 1) / threads.y);
+    sgemm<32><<<blocks, threads>>>(heightA, widthC, widthA, dTensorA, widthA, dTensorB, widthC, dTensorC, widthC);
+      
+    gettimeofday(&t_end, NULL);
+
+    times[i] = (t_end.tv_sec - t_start.tv_sec) * 1000.0f + (t_end.tv_usec - t_start.tv_usec) / 1000.0f;
+  }
+  
+  areTensorsEqual(dTensorC, dTensorCCublas, heightA * widthC) ? printf("\nTensors are equal\n") : printf("\nTensors are not equal\n");
+  
+  mean = 0.0f;
+  for (uint32_t i = samples; i--;) mean += times[i];
+  mean /= samples;
+  
+  qsort(times, samples, sizeof(float), compareFloats);
+  median = times[samples >> 1];
+  
+  printf("Mean: %f ms\n", mean);
+  printf("Median: %f ms\n", median);
+  
+  
+  
   cudaFree(dTensorA);
   cudaFree(dTensorB);
   cudaFree(dTensorC);
+  cudaFree(dTensorCCublas);
   
   checkCublasStatus(cublasDestroy(handle));
 
