136fcc576711486d8ba0156f36d4c3c9000432ec
theProgrammingBox <justinboxgea@gmail.com>
2024-02-05 15:54:00 -0800

    setting up for debugging

--- test3.cu
+++ test3.cu
@@ -4,8 +4,8 @@
 #include <sys/time.h>
 
 void mixSeed(uint32_t* seed1, uint32_t* seed2) {
-	*seed1 ^= (*seed2 >> 16) * 0xbf324c81;
-	*seed2 ^= (*seed1 >> 16) * 0x9c7493ad;
+	*seed1 ^= (*seed2 >> 13) * 0xbf324c81;
+	*seed2 ^= (*seed1 >> 7) * 0x9c7493ad;
 }
 
 void initSeed(uint32_t* seed1, uint32_t* seed2) {
@@ -25,23 +25,30 @@ __global__ void fillRand(uint32_t size, float *data, uint32_t seed1, uint32_t se
 	uint32_t hash;
 	for (uint32_t j = 0; j < itr; j++) {
 		idx = blockDim.x * j + i;
-		if (idx >= size) break;
-		hash = (idx ^ seed1) * 0xEDEF0EA3;
-		hash ^= ((hash >> 16) ^ seed2) * 0xC24F8CDB;
-		data[idx] = int32_t(hash) * 0.0000000004656612875245797f;
+		// if (idx >= size) break;
+		// hash = (idx ^ seed1) * 0xEDEF0EA3;
+		// hash ^= ((hash >> 13) ^ seed2) * 0xC24F8CDB;
+		// hash ^= hash << 17;
+		// data[idx] = int32_t(hash) * 0.0000000004656612875245797f;
+		data[idx] = -1;
 	}
+}
+
+void fillRand(uint32_t size, float *dataDev, uint32_t* seed1, uint32_t* seed2) {
+	fillRand<<<1, 1024>>>(size, dataDev, *seed1, *seed2);
+	mixSeed(seed1, seed2);
 }
 
 __global__ void prefixSum(uint32_t size, float *data, uint32_t jointSize, uint32_t elemOffset) {
 	uint32_t i = blockIdx.x * blockDim.x + threadIdx.x + 1;
-	uint32_t exp = 1;
+	uint32_t exp = jointSize;
 	uint32_t rep = ceil(size / float(blockDim.x));
 	uint32_t itr = floor(log2f(size));
 	uint32_t idx;
 	for (uint32_t j = 0; j < itr; j++) {
 		for (uint32_t k = 0; k < rep; k++) {
-			idx = (blockDim.x * k + i) * exp * 2 - 1;
-			if (idx >= size) break;
+			idx = (blockDim.x * k + i) * exp * 2 - jointSize;
+			if (idx >= size) break;	// 0th index, doesn't really check out of bounds in the element level, checks on the block leve;
 			data[idx] = data[idx] + data[idx - exp];
 		}
 		__syncthreads();
@@ -53,19 +60,22 @@ __global__ void prefixSum(uint32_t size, float *data, uint32_t jointSize, uint32
 		exp >>= 1;
 		rep <<= 1;	// not correct, but the break should compensate
 		for (uint32_t k = 0; k < rep; k++) {
-			idx = (blockDim.x * k + i) * exp * 2 - 1 + exp;
-			if (idx >= size) break;
+			idx = (blockDim.x * k + i) * exp * 2 - jointSize + exp;
+			if (idx >= size) break;	// if want to check true out of bounds as in not allocated, you need seperate error handling or idx + jointSize - 1 < size
 			data[idx] = data[idx] + data[idx - exp];
 		}
 		__syncthreads();
 	}
 }
 
-void printDataDev(float *dataDev, int size) {
+void printDataDev(float *dataDev, int size, int elems, uint32_t jointSize) {
 	float *data = (float *)malloc(size * sizeof(float));
 	cudaMemcpy(data, dataDev, size * sizeof(float), cudaMemcpyDeviceToHost);
-	for (int i = 0; i < size; i++) {
-		printf("%.2f, ", data[i]);
+	for (int i = 0; i < elems; i++) {
+		for (int j = 0; j < jointSize; j++) {
+			printf("%f, ", data[i * jointSize + j]);
+		}
+		printf("\n");
 	}
 	printf("\n");
 	free(data);
@@ -73,19 +83,19 @@ void printDataDev(float *dataDev, int size) {
 
 int main(int argc, char *argv[])
 {
-	const uint32_t size = 1025;
-	const uint32_t jointSize = 2;
+	const uint32_t elems = 1025;
+	const uint32_t jointSize = 3;
 	const uint32_t elemOffset = 1;
+	const uint32_t size = elems * jointSize;
 	uint32_t seed1, seed2;
 	initSeed(&seed1, &seed2);
 	
 	float *dataDev;
 	cudaMalloc(&dataDev, size * sizeof(float));
-	fillRand<<<1, 1024>>>(size, dataDev, seed1, seed2);
-	mixSeed(&seed1, &seed2);
+	fillRand(size, dataDev, &seed1, &seed2);
 	
 	prefixSum<<<1, 1024>>>(size, dataDev, jointSize, elemOffset);
-	printDataDev(dataDev, size);
+	printDataDev(dataDev, size, elems, jointSize);
 	cudaFree(dataDev);
 	
 	return 0;
