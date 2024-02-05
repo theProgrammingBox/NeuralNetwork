79f8805e02bbcbc6983db5fd3b05884de2094217
theProgrammingBox <justinboxgea@gmail.com>
2024-02-04 18:27:49 -0800

    added thread count

--- test3.cu
+++ test3.cu
@@ -8,26 +8,29 @@ __global__ void fillOne(int size, float *data) {
 	data[i] = 1;
 }
 
-__global__ void prefixSum(int size, float *data, int itr) {
+__global__ void prefixSum(int size, float *data, int itr, float r) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int threads = blockDim.x;
 	int temp;
 	int exp = 1;
+	int rep = ceil(r);
 	for (int j = 0; j < itr; j++) {
-		for (int k = 0; k < size; k++) {
-			temp = threads * k + i * exp * 2 - 1;
-			if (temp < size) {
-				data[temp] = data[temp] + data[temp - exp];
-			}
+		for (int k = 0; k < rep; k++) {
+			temp = (threads * k + i) * exp * 2 - 1;
+			if (temp >= size) break;
+			data[temp] = data[temp] + data[temp - exp];
 		}
 		__syncthreads();
-		exp *= 2;
+		exp <<= 1;
+		rep = ceil(rep / 2.0f);
 	}
 	
 	for (int j = 0; j < itr; j++) {
 		exp >>= 1;
-		temp = i * exp * 2 - 1 + exp;
-		if (temp < size) {
+		rep <<= 1;
+		for (int k = 0; k < rep; k++) {
+			temp = (threads * k + i) * exp * 2 - 1 + exp;
+			if (temp >= size) break;
 			data[temp] = data[temp] + data[temp - exp];
 		}
 		__syncthreads();
@@ -51,11 +54,11 @@ int main(int argc, char *argv[])
 	
 	cudaMalloc(&dataDev, size * sizeof(float));
 	
-	fillOne<<<1, 16>>>(size, dataDev);
+	fillOne<<<1, 1024>>>(size, dataDev);
 	
-	prefixSum<<<1, 16>>>(size, dataDev, floor(log2(size)));
+	prefixSum<<<1, 16>>>(size, dataDev, floor(log2(size)), (1023.0f / 16));
 	printDataDev(dataDev, size);
-	printf("floor(log2(size)) = %.0f\n", floor(log2(size)));
+	printf("floor(log2(size)) = %.0f, (1023.0f / 16) = %.0f\n", floor(log2(size)), (1023.0f / 16));
 	
 	cudaFree(dataDev);
 	
